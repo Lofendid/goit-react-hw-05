4a76880382d2d1415fc1fedbacbd650dd3b5bb2a
Daniil <panmeldin@gmail.com>
2024-03-11 17:27:57 +0100

    ready-to-start

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,43 @@
 // import { useState } from 'react';
 // import css from './App.module.css'
 
+import { Routes, Route } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+
+const HomePage = lazy(() => import('../../pages/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('../../pages/MoviesPage/MoviesPage'));
+const MovieDetailsPage = lazy(() =>
+  import('../../pages/MovieDetailsPage/MovieDetailsPage')
+);
+const NotFoundPage = lazy(() =>
+  import('../../pages/NotFoundPage/NotFoundPage')
+);
+
+const MovieReviews = lazy(() => import('../MovieReviews/MovieReviews'));
+const MovieCast = lazy(() => import('../MovieCast/MovieCast'));
+
+import Navigation from '../Navigation/Navigation';
+import Loading from '../Loading/Loading';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navigation />
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/movies" element={<MoviesPage />}></Route>
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />}>
+            <Suspense fallback={<Loading />}>
+              <Route path="reviews" element={<MovieReviews />}></Route>
+              <Route path="cast" element={<MovieCast />}></Route>
+            </Suspense>
+          </Route>
+          <Route path="/404" element={<NotFoundPage />}></Route>
+        </Routes>
+      </Suspense>
+    </>
+  );
 }
 
 export default App;
